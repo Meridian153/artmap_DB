6971cb298711ebd542b5c4d0369c57910e485b71
Cori1304-Seong <andyjm7739@gmail.com>
2026-05-23 21:22:44 +0900

    feat: ArtMovementRepository 인터페이스 및 구현체 추가

--- src/reception/domain/art_movement/__init__.py
+++ src/reception/domain/art_movement/__init__.py
@@ -1,3 +1,4 @@
 from src.reception.domain.art_movement.art_movement import ArtMovement
+from src.reception.domain.art_movement.repository import ArtMovementRepository
 
-__all__ = ["ArtMovement"]
+__all__ = ["ArtMovement", "ArtMovementRepository"]
